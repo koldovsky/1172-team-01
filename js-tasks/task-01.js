6684ed89ad80318ca6969361b19a597b174f18f9
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-04-17 20:25:24 +0300

    Create task-01.js

---
+++ js-tasks/task-01.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
