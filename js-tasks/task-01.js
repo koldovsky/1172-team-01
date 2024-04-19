98558001b144cba88cd4baec198ddf5e26064605
refokcer <refokcer@gmail.com>
2024-04-19 14:46:31 +0300

    redo 1 task

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -58,10 +58,7 @@ function greet(name, owner) {
 
 // Карандашов Артем робочий варіант
 function greet(name, owner) {
-  if (name == owner) {
-    return('Hello boss');
-  }
-  else return('Hello guest');
+  return name == owner ? 'Hello boss' :  'Hello guest';
 }
 
 
