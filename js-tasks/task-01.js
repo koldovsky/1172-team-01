253488f3b281a091468abf462b3ca2798acfb1e2
andriiberchak <138313305+andriiberchak@users.noreply.github.com>
2024-04-19 12:59:29 +0000

    Added solution for tasks

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -6,10 +6,14 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 // Карандашов Артем
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  let totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  let totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
   return totalGoals;
 }
 
+//Берчак Андрій
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
 
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 // Капиця Іван (chat GPT)
@@ -26,6 +30,10 @@ function makeNegative(number) {
   return -Math.abs(number);
 }
 
+//Берчак Андрій
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
 
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 // Капиця Іван
@@ -35,8 +43,13 @@ function move(position, roll) {
 
 // Карандашов Артем
 function move(position, roll) {
-  let movedToRightPosition = position + roll * 2
+  let movedToRightPosition = position + roll * 2;
   return movedToRightPosition;
+}
+
+//Берчак Андрій
+function move (position, roll) {
+  return position + roll * 2;
 }
 
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
@@ -58,23 +71,33 @@ function greet(name, owner) {
 
 // Карандашов Артем робочий варіант
 function greet(name, owner) {
-  return name == owner ? 'Hello boss' :  'Hello guest';
+  return name == owner ? "Hello boss" : "Hello guest";
 }
 
+//Берчак Андрій
+function greet (name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
 
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
 // Карандашов Артем
 function litres(time) {
-  var litres = Math.floor(time / 2)
+  var litres = Math.floor(time / 2);
   return litres;
+}
+
+//Берчак Андрій
+function litres(time) {
+  return Math.floor(0.5 * time);
 }
 
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 // Капиця Іван
 function lovefunc(flower1, flower2) {
   if (
-    (flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0)
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 === 0)
   ) {
     return true;
   } else {
@@ -83,9 +106,12 @@ function lovefunc(flower1, flower2) {
 }
 
 // Карандашов Артем
-function lovefunc(flower1, flower2){
-  if(flower1 % 2 != flower2 % 2)
-    return true
-  else return false
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 != flower2 % 2) return true;
+  else return false;
 }
 
+//Берчак Андрій
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2 ? true : false;
+}
