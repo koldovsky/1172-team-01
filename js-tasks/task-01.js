28654d0cffa5151810bda921842f56e470381156
refokcer <refokcer@gmail.com>
2024-04-19 16:23:46 +0300

    Edited file with JS tasks

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+// -------------------------------- https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript --------------------------------
 // Капиця Іван
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
@@ -15,7 +15,7 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+// ---------------------------------- https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript ------------------------------------
 // Капиця Іван (chat GPT)
 function makeNegative(number) {
   if (number > 0) {
@@ -35,7 +35,7 @@ function makeNegative(num) {
   return num < 0 ? num : -num;
 }
 
-// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+// -------------------------------- https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript ----------------------------
 // Капиця Іван
 function move(position, roll) {
   return position + roll * 2;
@@ -52,7 +52,7 @@ function move (position, roll) {
   return position + roll * 2;
 }
 
-// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+// --------------------------------- https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript --------------------------------
 // Капиця Іван (ні один з цих варіантів код варс не зархував. 1-ий писав сам, 2-ий чат GPT)
 function greet(name, owner) {
   if (name == owner) {
@@ -79,7 +79,7 @@ function greet (name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
 }
 
-// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+// -------------------------------- https://www.codewars.com/kata/keep-hydrated-1/train/javascript --------------------------------
 
 // Карандашов Артем
 function litres(time) {
@@ -92,7 +92,7 @@ function litres(time) {
   return Math.floor(0.5 * time);
 }
 
-// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+// -------------------------------- https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript  -------------------------------
 // Капиця Іван
 function lovefunc(flower1, flower2) {
   if (
