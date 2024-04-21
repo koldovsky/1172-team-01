c97097bcd8855a10c734ade2697326795fcb6aa0
refokcer <refokcer@gmail.com>
2024-04-21 23:52:00 +0300

    Create task02 JS and add some solves

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,28 +1,117 @@
-Write a function to split a string and convert it into an array of words
-Ігор Беспалов  
-def string_to_array(s):
-   return s.split(" ")
+// Write a function to split a string and convert it into an array of words
+// Ігор Беспалов  
+// def string_to_array(s):
+//    return s.split(" ")
 
-def DNAtoRNA(dna):
-  result = "";
-  dna = list(dna.upper());
-  for nucleotide in dna:
-    if nucleotide == "A":
-      result += "A";
-    if nucleotide == "T":
-      result += "U";
-    if nucleotide == "C":
-      result += "C";
-    if nucleotide == "G":
-      result += "G";
-  return result;
+// def DNAtoRNA(dna):
+//   result = "";
+//   dna = list(dna.upper());
+//   for nucleotide in dna:
+//     if nucleotide == "A":
+//       result += "A";
+//     if nucleotide == "T":
+//       result += "U";
+//     if nucleotide == "C":
+//       result += "C";
+//     if nucleotide == "G":
+//       result += "G";
+//   return result;
 
-def DNAtoRNA(dna):
-  result = "";
-  dna = list(dna.upper());
-  for nucleotide in dna:
-    if nucleotide == "T":
-      result += "U";
-    else:
-      result += nucleotide;
-  return result;
+// def DNAtoRNA(dna):
+//   result = "";
+//   dna = list(dna.upper());
+//   for nucleotide in dna:
+//     if nucleotide == "T":
+//       result += "U";
+//     else:
+//       result += nucleotide;
+//   return result;
+
+// -------------------------------- https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+// Карандашов Артем
+function stringToArray(string){
+  return string.split(" ");
+}
+
+// -------------------------------- https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/beginner-lost-without-a-map --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+
+
+
+
+
+// --------------------------------  https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+
+// -------------------------------- https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+// -------------------------------- https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
+
+
+// -------------------------------- https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript --------------------------------
+// Рішення:
+
+
+// ------------ Всі запропонованні варіанти рішень:
