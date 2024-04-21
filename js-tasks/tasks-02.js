a0664e951fd39751cf15bd8d3ab725eba940c94e
lenin892007 <165924383+lenin892007@users.noreply.github.com>
2024-04-21 08:41:45 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -2,3 +2,27 @@ Write a function to split a string and convert it into an array of words
 Ігор Беспалов  
 def string_to_array(s):
    return s.split(" ")
+
+def DNAtoRNA(dna):
+  result = "";
+  dna = list(dna.upper());
+  for nucleotide in dna:
+    if nucleotide == "A":
+      result += "A";
+    if nucleotide == "T":
+      result += "U";
+    if nucleotide == "C":
+      result += "C";
+    if nucleotide == "G":
+      result += "G";
+  return result;
+
+def DNAtoRNA(dna):
+  result = "";
+  dna = list(dna.upper());
+  for nucleotide in dna:
+    if nucleotide == "T":
+      result += "U";
+    else:
+      result += nucleotide;
+  return result;
