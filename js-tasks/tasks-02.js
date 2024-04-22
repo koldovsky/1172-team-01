443f226c528e5e25b30c05c2a76fe848a2cd3dc3
andriiberchak <138313305+andriiberchak@users.noreply.github.com>
2024-04-22 13:35:41 +0300

    Add tasks solution

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -52,6 +52,11 @@ function stringToArray(string){
   return string.split(" ");
 }
 
+//Берчак Андрій
+function stringToArray(string) {
+  return string.split(" ");
+}
+
 // -------------------------------- https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript --------------------------------
 // Рішення:
 
@@ -68,6 +73,11 @@ const DNAtoRNA = (dna) => dna.replaceAll("T", "U");
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, 'U');
  }
+
+//Берчак Андрій
+function DNAtoRNA(dna) {
+  return dna.replaceAll("T", "U");
+}
 
 // -------------------------------- https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript --------------------------------
 // Рішення:
@@ -112,6 +122,14 @@ function max (list) {
   return Math.max(...list);
 }
 
+//Берчак Андрій
+function min(list) {
+  return list.sort((a, b) => a - b)[0];
+};
+function max(list) {
+  return list.sort((a, b) => b - a)[0];
+};
+
 // -------------------------------- https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript --------------------------------
 // Рішення:
 
@@ -134,7 +152,19 @@ const min = (arr, toReturn) => toReturn === 'value' ? Math.min(...arr) : arr.ind
 
 //Валько Владислава
 function min(arr, toReturn) {
-  return toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));Ї
+  return toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+}
+
+//Берчак Андрій
+function min(arr, toReturn) {
+  if (toReturn === "value") {
+    return Math.min(...arr);
+  } else if (toReturn === "index") {
+    return arr.indexOf(Math.min(...arr));
+  }
+  return;
+}
+
 // -------------------------------- https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript --------------------------------
 // Рішення:
 
@@ -154,7 +184,20 @@ function warnTheSheep(queue) {
         "! You are about to be eaten by a wolf!";
   return message;
   }
-  
+
+//Берчак Андрій
+function warnTheSheep(queue) {
+  if (queue.pop() === "wolf") {
+    return "Pls go away and stop eating my sheep";
+  }
+  for (let i = queue.length - 1; i >= 0; i--) {
+    if (queue[i] === "wolf") {
+      return `Oi! Sheep number ${
+        queue.length - i
+      }! You are about to be eaten by a wolf!`;
+    }
+  }
+}
 
 // -------------------------------- https://www.codewars.com/kata/beginner-lost-without-a-map --------------------------------
 // Рішення:
@@ -171,6 +214,11 @@ function maps(x){
   return x.map (x => x*2);
   }
 
+//Берчак Андрій
+function maps(x) {
+  return x.map((num) => num * 2);
+}
+
 // -------------------------------- https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript --------------------------------
 // Рішення:
 
@@ -182,7 +230,15 @@ function firstNonConsecutive (arr) {
   return null;
 }
 
-
+//Берчак Андрій
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1];
+    }
+  }
+  return null;
+}
 
 
 
@@ -198,6 +254,11 @@ function doubleInteger(i) {
 }
 // Капиця Іван
 const doubleInteger = i => i * 2;
+
+//Берчак Андрій
+function doubleInteger(i) {
+  return i * 2;
+}
 // -------------------------------- https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript --------------------------------
 // Рішення:
 
@@ -205,6 +266,11 @@ const doubleInteger = i => i * 2;
 // ------------ Всі запропонованні варіанти рішень:
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+//Берчак Андрій
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(sonYearsOld * 2 - dadYearsOld);
 }
 
 // -------------------------------- https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript --------------------------------
@@ -218,6 +284,12 @@ function nthEven(n){
 }
 // Капиця Іван
 nthEven = (n) => n * 2 -2;
+
+//Берчак Андрій
+function nthEven(n) {
+  return n * 2 - 2;
+}
+
 // -------------------------------- https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript --------------------------------
 // Рішення:
 
@@ -228,6 +300,18 @@ function getRealFloor(n) {
   return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
 }
 
+//Берчак Андрій
+function getRealFloor(n) {
+  if (n === 1 || n === 0) {
+    return 0;
+  } else if (n < 0) {
+    return n;
+  } else if (n >= 13) {
+    return n - 2;
+  } else {
+    return n - 1;
+  }
+}
 
 // -------------------------------- https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript --------------------------------
 // Рішення:
@@ -239,6 +323,10 @@ function past(h, m, s){
   return h * 3600000 + m * 60000 + s * 1000;
 }
 
+//Берчак Андрій
+function past(h, m, s) {
+  return (h * 60 * 60 + m * 60 + s) * 1000;
+}
 
 // -------------------------------- https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript --------------------------------
 // Рішення:
@@ -246,6 +334,11 @@ function past(h, m, s){
 
 // ------------ Всі запропонованні варіанти рішень:
 // Карандашов Артем
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
+}
+
+//Берчак Андрій
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0;
 }
