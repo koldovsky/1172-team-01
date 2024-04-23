b3f0420191a95743a7abb3ca46d2ffb764cab520
refokcer <refokcer@gmail.com>
2024-04-23 12:07:22 +0300

    Created task 3 JS

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,32 +1,73 @@
-Given an array of integers your solution should find the smallest integer.
+// Given an array of integers your solution should find the smallest integer.
 
-For example:
+// For example:
 
-Given [34, 15, 88, 2] your solution will return 2
+// Given [34, 15, 88, 2] your solution will return 2
 
-Given [34, -345, -1, 100] your solution will return -345
+// Given [34, -345, -1, 100] your solution will return -345
 
-You can assume, for the purpose of this kata, that the supplied array will not be empty.
-  def find_smallest_int(arr):
-  smallest = arr[0];
-  for num in arr:
-    if num < smallest:
-      smallest = num;
-  return smallest;
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+//   def find_smallest_int(arr):
+//   smallest = arr[0];
+//   for num in arr:
+//     if num < smallest:
+//       smallest = num;
+//   return smallest;
 
-In lesson #11, we learned that the for loop can be used to traverse an array. If we want to traverse an object, we can use a variant of the for: for..in. This can traverse all the keys of the object. An example:
-function showObjectKeys(obj){
-  for (let key in obj){
-    console.log(key);
-  }
-}
-function showObjectValues(obj){
-  for (let key in obj){
-    console.log(obj[key]);
-  }
-}
-let arr=["one","two","three"];
-console.log("keys of arr:")
-showObjectKeys(arr);
-console.log("values of arr:")
-showObjectValues(arr);
+// In lesson #11, we learned that the for loop can be used to traverse an array. If we want to traverse an object, we can use a variant of the for: for..in. This can traverse all the keys of the object. An example:
+// function showObjectKeys(obj){
+//   for (let key in obj){
+//     console.log(key);
+//   }
+// }
+// function showObjectValues(obj){
+//   for (let key in obj){
+//     console.log(obj[key]);
+//   }
+// }
+// let arr=["one","two","three"];
+// console.log("keys of arr:")
+// showObjectKeys(arr);
+// console.log("values of arr:")
+// showObjectValues(arr);
+
+
+// -------------------------------- https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript --------------------------------
+// Рішення:
+
+
+// Всі запропонованні рішення:
+
+
+
+// -------------------------------- https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript --------------------------------
+// Рішення:
+
+
+// Всі запропонованні рішення:
+
+
+
+// -------------------------------- https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript --------------------------------
+// Рішення:
+
+
+// Всі запропонованні рішення:
+
+
+
+// -------------------------------- https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript --------------------------------
+// Рішення:
+
+
+// Всі запропонованні рішення:
+
+
+
+// -------------------------------- https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript --------------------------------
+// Рішення:
+
+
+// Всі запропонованні рішення:
+
+
