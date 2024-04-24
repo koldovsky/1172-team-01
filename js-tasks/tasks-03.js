e8a8e78133a9bcaa8971d03def151ad96c4ac104
refokcer <refokcer@gmail.com>
2024-04-24 12:34:07 +0300

    Karandashov Artem done task 03 JS

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -31,43 +31,81 @@
 // console.log("values of arr:")
 // showObjectValues(arr);
 
-
 // -------------------------------- https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript --------------------------------
 // Рішення:
 
-
 // Всі запропонованні рішення:
-
-
+// Карандашов Артем
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
 
 // -------------------------------- https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript --------------------------------
 // Рішення:
 
-
 // Всі запропонованні рішення:
-
-
+// Карандашов Артем
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}
 
 // -------------------------------- https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript --------------------------------
 // Рішення:
 
-
 // Всі запропонованні рішення:
-
-
+// Карандашов Артем
+function giveMeFive(obj) {
+  const arr = [];
+  for (let key in obj) {
+    if (key.length === 5) arr.push(key);
+    if (obj[key].length === 5) arr.push(obj[key]);
+  }
+  return arr;
+}
 
 // -------------------------------- https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript --------------------------------
 // Рішення:
 
-
 // Всі запропонованні рішення:
-
-
+// Карандашов Артем
+function buildFun(n) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(() => i);
+  }
+  return res;
+}
 
 // -------------------------------- https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript --------------------------------
 // Рішення:
 
-
 // Всі запропонованні рішення:
+// Карандашов Артем
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
 
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
 
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
