9f883bd469e76029b034890767908ac7766a3847
Albina <albina.grytsiv@gmail.com>
2024-04-25 18:34:03 +0300

    add to didribution section hover

--- js/index.section-benefits.partial.js
+++ js/index.section-benefits.partial.js
@@ -1,3 +1,8 @@
-document.querySelector(".vineyard-tours__button").addEventListener("click", function() {
+// document.querySelector(".vineyard-tours__button").addEventListener("click", function() {
+//     window.location.href = "contact_us.html";
+// });
+
+const buttonClick = document.querySelector(".vineyard-tours__button")
+buttonClick.addEventListener("click", function() {
     window.location.href = "contact_us.html";
 });
