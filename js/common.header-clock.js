eea69e49d17cc4f82fe4b7992e79545fa1c2e7e4
Julia Danilevych <danilevych.julia@gmail.com>
2024-04-27 22:46:24 +0300

    test clock

--- js/common.header-clock.js
+++ js/common.header-clock.js
@@ -1,7 +1,21 @@
 function updateClock() {
     const clockContainer = document.querySelector('.header__clock');
+    
+    const clockElement = document.createElement('div');
+    clockElement.innerText = new Date().toLocaleTimeString('uk');
+    clockContainer.innerHTML = '';
 
-    clockContainer.innerText = new Date().toLocaleTimeString('uk');
-};
+
+    clockContainer.appendChild(clockElement);
+
+    clockElement.addEventListener('mouseenter', function() {
+        clockElement.innerText = "Don't touch";
+    });
+
+    clockElement.addEventListener('mouseleave', function() {
+        clockElement.innerText = new Date().toLocaleTimeString('uk');
+    });
+}
 
 setInterval(updateClock, 1000);
+
