9da591306a169ce681725cedf52ea79e57f72cc5
refokcer <refokcer@gmail.com>
2024-05-08 00:19:49 +0300

    Do modal window

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function init() {
     import('./index.testimonials.js');
     import('./index.section-benefits.partial.js');
     import('./about_us.create_wines.js');
+    import('./modal-window.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
---
+++ js/modal-window.js
@@ -0,0 +1,4 @@
+// Get the modal
+var modal = document.getElementById("id01");
+
+document.getElementById("id01").style.display = "block";
